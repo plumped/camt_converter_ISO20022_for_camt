6c400a16579c6955e51f958ce56c88fcb7d76d52
5yn74x <41291962+plumped@users.noreply.github.com>
2023-05-10 18:36:54 +0200

    Optimization and fixing smaller issues

--- src/app.py
+++ src/app.py
@@ -1,13 +1,13 @@
+from flask import Flask, flash, render_template, request, redirect, url_for, send_file
 import os
-import xmlparser
-from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory
+
 from werkzeug.utils import secure_filename
 
+import xmlparser
 
 UPLOAD_FOLDER = "./uploads"
 DOWNLOAD_FOLDER = "./archives"
 ALLOWED_EXTENSIONS = {'zip'}
-
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -17,53 +17,54 @@ app.secret_key = "0ac1476b38841d44cee5e1d55d295c6c9625784634a50878e6c7c37cab10ca
 
 @app.route('/')
 def index():
-    xmlparser.filelist.clear()
+    xmlparser.clear_parsed_files()
     return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/uploaded', methods=['GET', 'POST'])
+
+@app.route('/uploaded')
 def uploaded():
-    items = xmlparser.filelist
-    if request.method == 'POST':
-        return redirect(url_for('download_file', name='csvperiban.zip'))
+    items = xmlparser.get_parsed_files()
     return render_template('uploaded.html', items=items)
 
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-        # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        # If the user does not select a file, the browser submits an
-        # empty file without a filename.
         if file.filename == '':
             flash('Please select a *.zip file first!')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            xmlparser.xml_parsing()
+            xmlparser.clear_parsed_files()
+            xmlparser.parse_xml_files()
             return redirect(url_for('uploaded'))
         else:
             flash('Wrong filetype! Please choose a *.zip file.')
             return redirect(request.url)
-    return
 
 
-@app.route('/archives/<name>')
-def download_file(name):
-    return send_from_directory(app.config["DOWNLOAD_FOLDER"], name)
+@app.route('/archives/<filename>', methods=['GET', 'POST'])
+def download_file(filename):
+    file_path = os.path.join(app.config["DOWNLOAD_FOLDER"], filename)
+    if os.path.exists(file_path):
+        return send_file(file_path, as_attachment=True)
+    else:
+        return f"{filename} does not exist."
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
--- src/xmlparser.py
+++ src/xmlparser.py
@@ -5,87 +5,115 @@ import zipfile
 import os
 import glob
 import shutil
-# import json
 
-dictibanentries = {}
+# A dictionary to store all the tags found in the input files
 alltags = {}
-ibanList = {}
-filelist = []
-ibandict = {}
-mylist = []
+
+allFiles = []
+
+# A dictionary with IBAN as the key and list of entries as the value
+ibanList = defaultdict(list)
+
+# Method to create a CSV file for each IBAN
 
 
-def createcsv(headers, ibanlists):
-    for iban in ibanList:
-        with open('./downloads/' + iban + ".csv", 'w', newline='', encoding='utf-8') as f:
-            filelist.clear()
-            filelist.append(iban)
+def create_csv(headers):
+    # Iterate over all IBANs and their entries
+    print("CREATECSV")
+    for iban, entries in ibanList.items():
+        print(iban)
+        # Create a CSV file with the name of the IBAN
+        allFiles.append(iban)
+        with open(f'./downloads/{iban}.csv', 'w', newline='', encoding='utf-8') as f:
             writer = csv.writer(f)
+            # Write the headers for the CSV file
             writer.writerow(headers)
-            for b in ibanlists[iban]:
-                writer.writerow(b.values())
+            # Write each entry as a row in the CSV file
+            for entry in entries:
+                writer.writerow(entry.values())
 
-        # makejson(entries)
+
+def clear_parsed_files():
+    allFiles.clear()
 
 
-def makearchive():  # Create zip archive from files in download folder
+def clear_iban_list():
+    ibanList.clear()
+
+
+def get_parsed_files():
+    return allFiles
+
+# Method to create a zip archive of all CSV files in the downloads folder
+
+
+def make_archive():
+    # Set the name of the archive file
     filename = "./archives/csvperiban"
+    # Set the format of the archive file
     format = "zip"
+    # Set the directory containing the CSV files to be archived
     directory = "./downloads"
+    # Create the archive file
     shutil.make_archive(filename, format, directory)
+    # Remove the uploads and downloads folders
     shutil.rmtree('./uploads/')
     shutil.rmtree('./downloads/')
+    # Recreate the uploads and downloads folders
     os.makedirs('./uploads')
     os.makedirs('./downloads')
+    print("-------------------------------------")
 
 
-# def makejson(entries):
-#     for list in entries:
-#         newlist = [x for x in list]
-#         with open('filename', 'w', encoding='utf8') as json_file:
-#             json.dump(newlist, json_file, ensure_ascii=False)
-
-
-def getalltags():
+def get_all_tags():
+    # set the path of the upload directory
     path = './uploads/'
+    # set an empty set to store the tags that are already seen
+    seen_tags = set()
+    # loop over each .zip file in the upload directory
     for filename in glob.glob(os.path.join(path, '*.zip')):
+        # read the content of the .zip file
         with zipfile.ZipFile(os.path.join(os.getcwd(), filename), 'r') as zf:
+            # loop over each file in the .zip archive
             for name in zf.namelist():
-                # Check for IBAN in filename
-                soup = BeautifulSoup(zf.open(name), "xml")
-
-                # Get all tags from input file and save it to list
+                # parse the XML file with BeautifulSoup
+                soup = BeautifulSoup(zf.open(name), 'xml')
+                # find all the tags in the XML file
                 tags = [tag.name for tag in soup.find_all()]
+                # add new tags to the dictionary of all tags
                 for tag in sorted(tags):
-                    if not tag in alltags:
-                        alltags[tag] = ""
+                    if tag not in seen_tags:
+                        alltags[tag] = ''
+                        seen_tags.add(tag)
 
-def xml_parsing():  # parse input xmls, find all entries, tags and ibans. sort them and put in single directories
-    # Import complete ZIP-Files
+
+def parse_xml_files():
+    # set path and get all tags
+    clear_iban_list()
     path = './uploads/'
-    getalltags()
+    get_all_tags()
+    # iterate through zip files in uploads directory
     for filename in glob.glob(os.path.join(path, '*.zip')):
         with zipfile.ZipFile(os.path.join(os.getcwd(), filename), 'r') as zf:
+            # iterate through xml files in the zip
             for name in zf.namelist():
-                soup = BeautifulSoup(zf.open(name), "xml")
+                # parse the xml file with BeautifulSoup
+                soup = BeautifulSoup(zf.open(name), 'xml')
                 iban = soup.find('IBAN')
                 x = iban.text
-                ibanList.setdefault(x, [])
-                dictibanentries.update({x: ""})
                 b_ntry = soup.find_all('Ntry')
                 for ntry in b_ntry:
-                    tagEntries = {}
+                    tag_entries = {}
+                    # iterate through all tags and find matching entries in xml file
                     for tag in alltags:
                         a = ntry.find(tag)
-                        if a:
-                            tagEntries[tag.title()] = a.text
+                        if tag == 'Cdtr':
+                            tag_entries[tag.title()] = a.find('Nm').text if a and a.find('Nm') else ''
+                        elif tag == 'Dbtr':
+                            tag_entries[tag.title()] = a.find('Nm').text if a and a.find('Nm') else ''
                         else:
-                            tagEntries[tag.title()] = ""
-                    ibanList[x] += [tagEntries]
-        createcsv(alltags, ibanList)
-    # makearchive()
-
-
-xml_parsing()
-print(alltags)
-# AKTUELL 08.05.2023 20:18
+                            tag_entries[tag.title()] = a.text if a else ''
+                    ibanList[x].append(tag_entries)
+            zf.close()
+        create_csv(alltags)
+    make_archive()
